e4fce74ce45f6d04d4b9c23abcd27c6ff27ef1da
Dmitry [KP0H™] Pelevin <dmitry@pelevin.pro>
2021-05-08 20:44:59 +0300

    ~

---
+++ MCB.VBO.Microservices/MCB.VBO.Microservices.Statements/Controllers/StatementController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+
+namespace MCB.VBO.Microservices.Statements.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatementController : ControllerBase
+    {
+        private readonly ILogger<StatementController> _logger;
+
+        private readonly IWebHostEnvironment _appEnvironment;
+
+        public StatementController(ILogger<StatementController> logger, IWebHostEnvironment appEnvironment)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("create")]
+        public int Create()
+        {
+            return 0;
+        }
+
+        [HttpGet("status")]
+        public int GetStatus()
+        {
+            return 0;
+        }
+
+        [HttpGet("download")]
+        public IActionResult GetStatementFile()
+        {
+            HttpClient httpClient = new HttpClient();
+            templateServiceClient client = new templateServiceClient("https://localhost:55002/swagger", httpClient);
+
+            var result = client.WeatherForecastAsync();
+            result.Wait();
+
+            //return result.Result;
+            return new JsonResult("meh");
+        }
+
+        [HttpGet("history")]
+        public IEnumerable<string> GetHistory()
+        {
+            return new[] { "1", "2", "3" };
+        }
+    }
+}
---
+++ MCB.VBO.Microservices/MCB.VBO.Microservices.Templates/Controllers/TemplatesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+
+namespace MCB.VBO.Microservices.Templates.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemplatesController : ControllerBase
+    {
+        private readonly ILogger<TemplatesController> _logger;
+
+        public TemplatesController(ILogger<TemplatesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Путь к файлу
+            string file_path = Path.Combine(Environment.CurrentDirectory, "temp.pdf");
+            // Тип файла - content-type
+            string file_type = "application/pdf";
+            // Имя файла - необязательно
+            string file_name = "book.pdf";
+
+            return PhysicalFile(file_path, file_type, file_name);
+        }
+    }
+}
